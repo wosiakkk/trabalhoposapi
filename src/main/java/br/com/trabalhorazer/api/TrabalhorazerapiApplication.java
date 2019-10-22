e0f469b3fb82fb28b011e787000b479997e35299
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-10-21 21:42:55 -0300

    first commit

---
+++ src/main/java/br/com/trabalhorazer/api/TrabalhorazerapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.trabalhorazer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhorazerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhorazerapiApplication.class, args);
+	}
+
+}
